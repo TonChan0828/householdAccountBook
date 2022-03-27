60f3fef46cff2cb1009da591ca05c2c82a129a82
TonChan0828 <showxillia1028@yahoo.co.jp>
2022-03-27 23:09:56 +0900

    収支を表示するグラフを追加

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,6 +21,43 @@
         </div>
         <div class="col-6">
             <h3>収支：{{ $selectData['incomeSum'] - $selectData['outgoSum'] }}円</h3>
+
+            <canvas id="myBarChart"></canvas>
+            // <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js"></script>
+            <script>
+                const ctx = document.getElementById("myBarChart");
+                const myBarChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['収支'],
+                    datasets: [
+                        {
+                            label: '収入',
+                            data: [{{ $selectData['incomeSum'] }}],
+                            backgroundColor: "rgba(130,201,169,0.5)"
+                        },{
+                            label: '支出',
+                            data: [{{ $selectData['outgoSum'] }}],
+                            backgroundColor: "rgba(219,39,91,0.5)"
+                        },
+                    ]
+                },
+                options: {
+                    scales: {
+                    yAxes: [{
+                        ticks: {
+                        // suggestedMax: 20000,
+                        suggestedMin: 0,
+                        stepSize: 5000,
+                        callback: function(value, index, values){
+                            return  value +  '円'
+                        }
+                        }
+                    }]
+                    },
+                }
+                });
+            </script>
         </div>
     </div>
 @endsection
